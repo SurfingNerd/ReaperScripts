dbf9364eed0d35ac45dd94d0587ecf363a901e57
Thomas Haller <Thomas.Haller@lab10.coop>
2019-12-31 12:34:25 +0100

    removd debug output, performance increase?

--- CleanupCC.lua
+++ CleanupCC.lua
@@ -20,7 +20,7 @@ function enumAllCCMidis()
       retval, midiCC.selected, midiCC.muted, midiCC.ppqpos, midiCC.chanmsg, midiCC.chan, midiCC.msg2, midiCC.msg3 = reaper.MIDI_GetCC(take, ccidx)
       getEventResult = {}
       retval, getEventResult.selected, getEventResult.muted, getEventResult.ppqpos, getEventResult.msg = reaper.MIDI_GetEvt(take, ccidx, midiCC.selected, midiCC.muted, midiCC.ppqpos,'')
-      reaper.ShowConsoleMsg(getEventResult.msg .. ' - ppqpos  ' .. tostring(midiCC.ppqpos) .. ' chanmsg ' .. tostring(midiCC.chanmsg) .. ' chan ' .. tostring(midiCC.chan) .. ' msg2 ' .. tostring(midiCC.msg2) .. ' msg3 ' .. midiCC.msg3 .. '\n')
+      -- reaper.ShowConsoleMsg(getEventResult.msg .. ' - ppqpos  ' .. tostring(midiCC.ppqpos) .. ' chanmsg ' .. tostring(midiCC.chanmsg) .. ' chan ' .. tostring(midiCC.chan) .. ' msg2 ' .. tostring(midiCC.msg2) .. ' msg3 ' .. midiCC.msg3 .. '\n')
       if not (midiCC.msg2 == 1 or midiCC.msg2 == 11) then
         -- reaper.ShowConsoleMsg('indexesNumber' .. tostring(indexesNumber) .. '  ' .. tostring(ccidx) .. ' msg2 value: ' .. tostring(midiCC.msg2) .. '\n')
         indexes2Delete[indexesNumber] = ccidx
@@ -33,7 +33,7 @@ function enumAllCCMidis()
       indexesNumber = indexesNumber - 1
       local ccidx = indexes2Delete[indexesNumber]
       
-      reaper.ShowConsoleMsg('deleteing # ' .. tostring(indexesNumber) .. ' on index ' .. tostring(ccidx) .. '\n')
+      -- reaper.ShowConsoleMsg('deleteing # ' .. tostring(indexesNumber) .. ' on index ' .. tostring(ccidx) .. '\n')
       local deleteResult = reaper.MIDI_DeleteCC(take, ccidx)
       if not deleteResult then
         reaper.ShowConsoleMsg('Unable to delete CC on ' .. tostring(ccidx))
